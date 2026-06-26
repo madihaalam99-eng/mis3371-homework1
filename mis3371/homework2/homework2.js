23742482b1bd243b31ed0b3c6359a7bbc3dc6ae7
madihaalam99-eng <madihaalam99@gmail.com>
2026-06-26 17:38:17 -0500

    Update homework2.js

--- mis3371/homework2/homework2.js
+++ mis3371/homework2/homework2.js
@@ -1,7 +1,19 @@
 window.onload = function () {
     document.getElementById("todayDate").innerHTML =
         new Date().toDateString();
+
+    showRating();
 };
+
+function showRating() {
+    var slider = document.getElementById("healthRating");
+    var output = document.getElementById("ratingValue");
+
+    if (slider && output) {
+        output.innerHTML = slider.value;
+    }
+}
+
 function getValue(id) {
     var field = document.getElementById(id);
     return field ? field.value : "";
@@ -9,27 +21,61 @@ function getValue(id) {
 
 function getRadioValue(name) {
     var radios = document.getElementsByName(name);
+
     for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) return radios[i].value;
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
     }
-    return "Not selected";
+
+    return "Not Selected";
 }
 
 function reviewForm() {
+
     var output = "";
 
-    output += "<p><b>Name:</b> " + getValue("firstName") + " " + getValue("middleInitial") + " " + getValue("lastName") + "</p>";
-    output += "<p><b>Date of Birth:</b> " + getValue("dob") + "</p>";
-    output += "<p><b>SSN:</b> " + getValue("ssn") + "</p>";
-    output += "<p><b>Address:</b> " + getValue("address1") + " " + getValue("address2") + "</p>";
-    output += "<p><b>City/State/Zip:</b> " + getValue("city") + ", " + getValue("state") + " " + getValue("zip") + "</p>";
-    output += "<p><b>Email:</b> " + getValue("email") + "</p>";
-    output += "<p><b>Symptoms:</b> " + getValue("symptoms") + "</p>";
-    output += "<p><b>Gender:</b> " + getRadioValue("gender") + "</p>";
-    output += "<p><b>Vaccinated:</b> " + getRadioValue("vaccinated") + "</p>";
-    output += "<p><b>Insurance:</b> " + getRadioValue("insurance") + "</p>";
-    output += "<p><b>User ID:</b> " + getValue("userId").toLowerCase() + "</p>";
-    output += "<p><b>Password:</b> " + getValue("password") + "</p>";
+    output += "<p><b>Name:</b> " +
+        getValue("firstName") + " " +
+        getValue("middleInitial") + " " +
+        getValue("lastName") + "</p>";
+
+    output += "<p><b>Date of Birth:</b> " +
+        getValue("dob") + "</p>";
+
+    output += "<p><b>SSN:</b> " +
+        getValue("ssn") + "</p>";
+
+    output += "<p><b>Address:</b> " +
+        getValue("address1") + " " +
+        getValue("address2") + "</p>";
+
+    output += "<p><b>City:</b> " +
+        getValue("city") + "</p>";
+
+    output += "<p><b>State:</b> " +
+        getValue("state") + "</p>";
+
+    output += "<p><b>Zip:</b> " +
+        getValue("zip") + "</p>";
+
+    output += "<p><b>Email:</b> " +
+        getValue("email") + "</p>";
+
+    output += "<p><b>Symptoms:</b> " +
+        getValue("symptoms") + "</p>";
+
+    output += "<p><b>Gender:</b> " +
+        getRadioValue("gender") + "</p>";
+
+    output += "<p><b>Vaccinated:</b> " +
+        getRadioValue("vaccinated") + "</p>";
+
+    output += "<p><b>Insurance:</b> " +
+        getRadioValue("insurance") + "</p>";
+
+    output += "<p><b>User ID:</b> " +
+        getValue("userId") + "</p>";
 
     document.getElementById("reviewOutput").innerHTML = output;
 }
